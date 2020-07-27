52bc3c99bd49a0b0d4f7d97ee18eb8bc7db3d1aa
Emanuel Medeiros <emanuelmbs@protonmail.com>
2020-07-27 11:50:47 -0300

    Update 'domain' var to array and move to TLS 1.2 (2019)

--- r53-cname/main.tf
+++ r53-cname/main.tf
@@ -2,10 +2,13 @@
 ## Create a Route53 CNAME record to the Cloudfront distribution
 ################################################################################################################
 resource "aws_route53_record" "cdn-cname" {
-  zone_id = var.route53_zone_id
-  name    = var.domain
-  type    = "CNAME"
-  ttl     = "300"
-  records = [var.target]
-}
 
+  for_each = [for cname in var.domain: {
+    zone_id = var.route53_zone_id
+    name    = cname
+    type    = "CNAME"
+    ttl     = "300"
+    records = [var.target]    
+  }]
+  
+}
